0cb60ee5222c4a334416b471509832548115f2f4
Brk <bartosz.nowak@polcode.net>
2018-01-19 15:00:56 +0100

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
  $(document).ready(function () {
 
-     function toggleIcon(e) {
-         $(e.target)
-             .prev('.panel-heading')
-             .find(".more-less")
-             .toggleClass('glyphicon-plus glyphicon-minus');
-     }
-     $('.panel-group').on('hidden.bs.collapse', toggleIcon);
-     $('.panel-group').on('shown.bs.collapse', toggleIcon);
+   function toggleIcon(e) {
+     $(e.target)
+       .prev('.panel-heading')
+       .find(".more-less")
+       .toggleClass('glyphicon-plus glyphicon-minus');
+   }
+   $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+   $('.panel-group').on('shown.bs.collapse', toggleIcon);
 
+   $('select').niceSelect();
  });
